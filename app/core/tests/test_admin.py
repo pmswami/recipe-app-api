1d932e9a925f70f53133f99ca93a99e1f9dad865
swamfire <usern5949@gmail.com>
2023-08-03 23:03:46 +0530

    added test case for new user addition

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -36,3 +36,9 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test create new user page works as expected"""
+        url = reverse("admin:core_user_add")
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
