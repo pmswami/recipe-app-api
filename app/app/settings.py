3639d7a5d62caaa762a5a736aea8d11fb46aff08
swamfire <usern5949@gmail.com>
2023-08-03 21:05:29 +0530

    fixed host name resolution

--- app/app/settings.py
+++ app/app/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("DB_NAME"),
-        'HOST': os.environ.get("DB_NAME"),
+        'HOST': os.environ.get("DB_HOST"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASS")
     }
