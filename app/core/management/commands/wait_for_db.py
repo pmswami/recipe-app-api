d13d8ff5952a5571f2aebe8670519767193ee42d
swamfire <usern5949@gmail.com>
2023-08-03 17:41:21 +0530

    added migrate inclusion

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -15,12 +15,17 @@ class Command(BaseCommand):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
+        count = 0
         while db_up is False:
+            count+=1
             try:
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
+                if(count == 10):
+                    self.stdout.write('Unable to establish conection with DB')
+                    exit()
 
         self.stdout.write('Database available!')
