e3ecef7252fdb25f45955cb82382a3ddbcdc9742
swamfire <usern5949@gmail.com>
2023-08-05 05:12:14 +0530

    implimented user APIs

--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 Views for user API
 """
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -20,3 +20,13 @@ class CreateTokenView(ObtainAuthToken):
     """Creates an auth token to access other endpoints of our app"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated users profile"""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        return self.request.user
