76f82ff04d7579cd1f644f5d940aceee0c4c4363
swamfire <usern5949@gmail.com>
2023-08-05 03:30:31 +0530

    Implimented token apis

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -3,7 +3,11 @@ Serializers for User API views
 """
 
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import (
+    get_user_model,
+    authenticate,
+)
+from django.utils.translation import gettext as _
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
@@ -18,3 +22,26 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """ create and return a suer with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+class AuthTokenSerializer(serializers.Serializer):
+    """ Serializer for creating an auth token"""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+    )
+
+    def validate(self, attrs):
+        """ Validate and authenticate the user"""
+        email = attrs["email"]
+        password = attrs["password"]
+        user = authenticate(
+            request=self.context.get("request"),
+            username=email,
+            password=password
+        )
+        if not user:
+            msg = _("Unable to log in with provided credentials")
+            raise serializers.ValidationError(msg, code="authentication")
+        attrs['user'] = user
+        return attrs
--- app/user/views.py
+++ app/user/views.py
@@ -3,8 +3,20 @@ Views for user API
 """
 
 from rest_framework import generics
-from user.serializers import UserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+)
+
 
 class CreateUserView(generics.CreateAPIView):
     """Creates a new user in the system."""
     serializer_class = UserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+    """Creates an auth token to access other endpoints of our app"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
