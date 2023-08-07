34ee8effc91289dc2f5a605db68fde052fffa00e
swamfire <usern5949@gmail.com>
2023-08-07 21:19:18 +0530

    Adde recipe details test cases

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -9,9 +9,17 @@ from rest_framework import status
 from rest_framework.test import APIClient
 
 from core.models import Recipe
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import (
+    RecipeSerializer,
+    RecipeDetailSerializer,
+)
 
 RECIPE_URL = reverse("recipe:recipe-list")
+
+def detail_url(recipe_id):
+    """Return recipe details URL"""
+    return reverse("receipe:recipe-detail", args=[recipe_id])
+
 
 def create_recipe(user, **params):
     """Create a recipe for the given user."""
@@ -76,3 +84,13 @@ class PrivateRecipeApiTests(TestCase):
         serializer = RecipeSerializer(recipes,many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_get_recipe_detail(self):
+        """Test getting the details of an individual recipe"""
+        recipe = create_recipe(user=self.user)
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
+        self.assertEqual(res.data,serializer.data)
