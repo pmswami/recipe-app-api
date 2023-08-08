f33afa1cf9998585ccbde43337fc08dc215b9b1f
swamfire <usern5949@gmail.com>
2023-08-09 00:39:35 +0530

    Implimented tags delete test case

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,6 +38,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 class TagViewSet(
+    mixins.DestroyModelMixin,
     mixins.UpdateModelMixin,
     mixins.ListModelMixin,
     viewsets.GenericViewSet):
