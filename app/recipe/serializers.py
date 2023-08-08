9bd00c126465169e62bce6fc9eb38f4f6e425d97
swamfire <usern5949@gmail.com>
2023-08-09 01:42:42 +0530

    Implimented recipe creation with tags test case

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -9,23 +9,41 @@ from core.models import (
     Tag,
 )
 
-class RecipeSerializer(serializers.ModelSerializer):
-    """Serializes a recipe object"""
-
-    class Meta:
-        model = Recipe
-        fields = ['id', 'title', "time_minutes", "price", "link"]
-        read_only_fields = ["id"]
-
-class RecipeDetailSerializer(RecipeSerializer):
-    """Serialize a recipe detail"""
-    class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
-
-
 class TagSerializer(serializers.ModelSerializer):
     """Serailizer for tag objects"""
+
     class Meta:
         model=Tag
         fields=["id", "name"]
         read_only_field=['id']
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializes a recipe object"""
+
+    tags = TagSerializer(many=True, required=False)
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', "time_minutes", "price", "link", "tags"]
+        read_only_fields = ["id"]
+
+    def create(self, validated_data):
+        """Create and return new recipe"""
+        tags = validated_data.pop("tags", [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context["request"].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
+
+
+
