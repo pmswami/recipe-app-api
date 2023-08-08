b2dd0ef0c45fe9a77938e6dbb8bc05c538be18fa
swamfire <usern5949@gmail.com>
2023-08-09 00:10:26 +0530

    Added test cases for Tag model

---
+++ app/recipe/tests/test_tags_api.py
@@ -0,0 +1,60 @@
+"""
+Test for Tags API
+"""
+
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+from recipe.serializers import TagSerializer
+
+TAGS_URL = reverse("recipe:tags-list")
+
+def create_user(email="test@example.com", password="testpass123"):
+    """Create a new user with email and passowrd."""
+    return get_user_model().objects.create_user(email=email, password=password)
+
+class PublicTagsApiTests(TestCase):
+    """Test unauthenticated API requests."""
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test auth is required for retrieving tags"""
+        res = self.client.get(TAGS_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+class PrivateTagsApiTests(TestCase):
+    """Test authenticated tag api request."""
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieve all the tags in db by authorized user"""
+        # Create two tags to be retrieved from database
+        Tag.objects.create(user=self.user, name='Vegan')
+        Tag.objects.create(user=self.user, name='Dessert')
+        res = self.client.get(TAGS_URL)
+        tags = Tag.objects.all().order_by('-name')
+        serializer = TagSerializer(tags, many=True)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        """Test that only users can view their own tags"""
+        other_user = create_user('user2@example.com')
+        # Create three tags as different users
+        Tag.objects.create(user=other_user, name='Fruity')
+        # Test that we cannot see this tag because it's not ours!
+        tag = Tag.objects.create(user=self.user, name='Sweet')
+        res = self.client.get(TAGS_URL)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data[0]["name"], tag.name)
+        self.assertEqual(res.data[0]["id"], tag.id)
