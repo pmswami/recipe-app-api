4c925a107d9b7325ad5140074e60c0d281638600
swamfire <usern5949@gmail.com>
2023-08-08 21:10:49 +0530

    Added tags test case

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,6 +7,11 @@ from django.contrib.auth import get_user_model
 from decimal import Decimal
 from core import models
 
+def create_user(email="test@example.com", password="testpass123"):
+    """Create and return a new user"""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """ Test models """
 
@@ -65,3 +70,9 @@ class ModelTests(TestCase):
             description = "Sample Recipe Description",
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating tag and saving it to database successfully"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name="tag1")
+        self.assertEqual(str(tag), tag.name)
