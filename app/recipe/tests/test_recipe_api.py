e06ef469ea8080e60dce018ba15fb77ffabe1641
swamfire <usern5949@gmail.com>
2023-08-10 02:23:47 +0530

    Added create test cases for ingredients

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -284,7 +284,7 @@ class PrivateRecipeApiTests(TestCase):
         }
         res = self.client.post(RECIPE_URL,payload ,format="json")
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        recipes = Recipe.objects.get(user=self.user)
+        recipes = Recipe.objects.filter(user=self.user)
         self.assertEqual(recipes.count(), 1)
         recipe = recipes[0]
         self.assertEqual(recipe.ingredients.count(), 2)
