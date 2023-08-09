c524f24776dea263ab23eb90823d94ec082ec16e
swamfire <usern5949@gmail.com>
2023-08-09 23:41:35 +0530

    added update test cases for ingredients

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -14,12 +14,18 @@ from recipe.serializers import IngredientSerializer
 
 INGREDIENTS_URL = reverse("recipe:ingredient-list")
 
+def detail_url(ingredient_id):
+    """Return ingredient details URL"""
+    return reverse("recipe:ingredient-detail", args=[ingredient_id])
+
+
 def create_user(email="user@example.com", password="testpass123"):
     """Create a new user."""
     return get_user_model().objects.create_user(
         email=email,
         password=password
     )
+
 
 class PublicIngredientsApiTests(TestCase):
     """ Test unauthenticated API requests """
@@ -30,6 +36,7 @@ class PublicIngredientsApiTests(TestCase):
         """Test auth is required for retrieving ingredients"""
         res = self.client.get(INGREDIENTS_URL)
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
 
 class PrivateIngredientsApiTests(TestCase):
     """ Test authenticated API requestss """
@@ -62,3 +69,15 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]["name"], ingredient.name)
         self.assertEqual(res.data[0]["id"], ingredient.id)
+
+    def test_update_ingredient(self):
+        """Test updating an ingredient"""
+        ingredient = Ingredient.objects.create(user=self.user, name="Cilantro")
+        payload = {"name": "Coriander"}
+
+        url = detail_url(ingredient.id)
+        res = self.client.patch(url,payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        ingredient.refresh_from_db()
+        self.assertEqual(ingredient.name, payload["name"])
