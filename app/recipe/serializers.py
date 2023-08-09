f8092dbb33d88c5d83f54a5ffc7462319220989c
swamfire <usern5949@gmail.com>
2023-08-09 21:44:09 +0530

    Implimented test cases for updating tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -27,10 +27,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', "time_minutes", "price", "link", "tags"]
         read_only_fields = ["id"]
 
-    def create(self, validated_data):
-        """Create and return new recipe"""
-        tags = validated_data.pop("tags", [])
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, tags, recipe):
+        """Get or Create Tags"""
         auth_user = self.context["request"].user
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(
@@ -38,7 +36,32 @@ class RecipeSerializer(serializers.ModelSerializer):
                 **tag,
             )
             recipe.tags.add(tag_obj)
+
+    def create(self, validated_data):
+        """Create and return new recipe"""
+        tags = validated_data.pop("tags", [])
+        recipe = Recipe.objects.create(**validated_data)
+        # auth_user = self.context["request"].user
+        # for tag in tags:
+        #     tag_obj, created = Tag.objects.get_or_create(
+        #         user=auth_user,
+        #         **tag,
+        #     )
+        #     recipe.tags.add(tag_obj)
+        self._get_or_create_tags(tags, recipe)
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update an existing recipe"""
+        tags = validated_data.pop("tags", None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serialize a recipe detail"""
