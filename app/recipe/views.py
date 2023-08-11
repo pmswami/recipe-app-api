051618effd0e2c0a68083034460dd5a9dbec5bd9
swamfire <usern5949@gmail.com>
2023-08-12 02:55:51 +0530

    added filtering by recipes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -91,6 +91,19 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+#Extend the default schema of drf_spectacular
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "assigned_only",
+                OpenApiTypes.INT,
+                enum=[0,1],
+                description="A comma-separated list of tags.",
+                )
+        ]
+    )
+)
 class BaseRecipeAttrViewSet(
     mixins.DestroyModelMixin,
     mixins.UpdateModelMixin,
@@ -102,7 +115,14 @@ class BaseRecipeAttrViewSet(
 
     def get_queryset(self):
         """Filter querysets by authenicated user"""
-        return self.queryset.filter(user=self.request.user).order_by("-name")
+        assigned_only = bool(
+            int(self.request.query_params.get("assigned_only", 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(user=self.request.user).order_by("-name").distinct()
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
