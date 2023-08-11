70624d0c08018883fb2b44adee3d221e01831c5c
swamfire <usern5949@gmail.com>
2023-08-12 02:13:21 +0530

    Implimented test cases for filtering

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -17,6 +17,12 @@ from core.models import (
     Tag,
     Ingredient,
 )
+from drf_spectacular.utils import (
+    extend_schema,
+    extend_schema_view,
+    OpenApiTypes,
+    OpenApiParameter,
+)
 from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -26,9 +32,25 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
+    def _params_to_ints(self, qs):
+        """Convert string IDs to integers"""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         """Retrieve the authenticated user's recipes"""
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        tags = self.request.query_params.get("tags")
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        # Filter by tag name
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        # Filter by ingredients
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(user=self.request.user).order_by('-id').distinct()
 
     def get_serializer_class(self):
         """Return the serializer class for request"""
