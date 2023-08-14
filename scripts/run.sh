ee198120c69b62b3aee021f837a22ee8eeeec2a2
swamfire <usern5949@gmail.com>
2023-08-15 02:53:58 +0530

    upgraded packages

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,3 @@
-# #!/bin/sh
-
-# set -e
-
-# python manage.py wait_for_db # wait till db gets ready for new connections
-# python manage.py collectstatic --noinput #collect all static files
-# python manage.py migrate #migrate all changes to db
-# uwsgi --socket :9000 --workers 10 --master --enable-threads --module app.wsgi
-
-
 #!/bin/sh
 
 set -e
